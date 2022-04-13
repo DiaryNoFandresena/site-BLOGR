837275f15489a0a999f33193a101bae8b523dad2
Diary <diaryrandrianars@gmail.com>
2022-04-13 14:07:20 +0300

    premier commit

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+let dropDown=document.querySelectorAll('.listeAll');  
+let liste_product=document.querySelectorAll('.listeAll_element');
+let Body=document.querySelector("body");
+for(let i=0 ; i<dropDown.length ; i++){
+    dropDown[i].addEventListener('click',function (){
+        if(liste_product[i].style.display!=="block"){
+            liste_product[i].style.display="block";
+            dropDown[i].style.fontWeight="bold";
+        }
+        else liste_product[i].style.display="none";
+    })
+    Body.addEventListener('click', function(e){
+        if(e.target!==dropDown[i]){
+            liste_product[i].style.display="none";
+            dropDown[i].style.fontWeight="normal"; 
+        }
+    })
+}
+
